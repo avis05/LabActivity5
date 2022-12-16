fccfc7a290fcd20850b8f930242df0ec25582381
Andrhey Visbal <andrheyvisbal26@gmail.com>
2022-12-16 16:53:28 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,9 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel pnlCalculator;
@@ -9,7 +12,14 @@ public class SimpleCalcGUI extends JFrame{
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
-
+    public SimpleCalcGUI(){
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    compute();
+                }
+        });
+    }
     public static void main(String[] args) {
         SimpleCalcGUI cal = new SimpleCalcGUI();
         cal.setContentPane(cal.pnlCalculator);
@@ -17,5 +27,27 @@ public class SimpleCalcGUI extends JFrame{
         cal.setDefaultCloseOperation(EXIT_ON_CLOSE);
         cal.setVisible(true);
         cal.setTitle("Simple Calculator");
+    }
+    public void compute(){
+        String selectedOp = Objects.requireNonNull(cbOperations.getSelectedItem()).toString();
+        int num1 = Integer.parseInt(tfNumber1.getText());
+        int num2 = Integer.parseInt(tfNumber2.getText());
+        int result = 0;
+
+        switch (selectedOp){
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+        lblResult.setText(String.valueOf(result));
     }
 }
